22dec1a050f456626d983f3a7df747155a7c3d2e
Chris <chris@gamekombinat.com>
2022-05-30 10:32:49 +0200

    The good old "IsFacing" shenanigans
    I might add vector extensions to that later for the comfort

---
+++ Runtime/Broilerplate/Tools/VectorTools.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Broilerplate.Tools {
+    public static class VectorTools {
+        public static float Dot(Vector3 direction, Vector3 targetDirection) {
+            return Vector3.Dot(direction, targetDirection);
+        }
+
+        public static bool IsFacing(Vector3 direction, Vector3 fromHere, Vector3 facingThis, float precision = .55f) {
+            return Dot(direction, (facingThis - fromHere).normalized) >= precision;
+        }
+        
+        public static bool IsFacingWithinAngle(Vector3 direction, Vector3 fromHere, Vector3 facingThis, float angle = 15f) {
+            float dot = Dot(direction, (facingThis - fromHere).normalized);
+            return Mathf.Acos(dot) * Mathf.Rad2Deg <= angle;
+        }
+        
+        public static bool IsFacingWithinAngle(Vector3 direction, Vector3 targetDirection, float angle = 15f) {
+            float dot = Dot(direction, targetDirection);
+            return Mathf.Acos(dot) * Mathf.Rad2Deg <= angle;
+        }
+    }
+}
