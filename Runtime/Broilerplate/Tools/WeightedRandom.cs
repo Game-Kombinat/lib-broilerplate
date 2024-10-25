2af3129502580c1971502fff5437f8eb8129764d
Chris Ksoll <chris@gamekombinat.com>
2024-10-25 11:50:45 +0200

    Extending weighted random extension a bit
    Allowing to pass down a custom random instance

--- Runtime/Broilerplate/Tools/WeightedRandom.cs
+++ Runtime/Broilerplate/Tools/WeightedRandom.cs
@@ -9,11 +9,15 @@ namespace Broilerplate.Tools {
     public static class WeightedRandom {
         private static readonly Random Random = new Random();
         public static T Get<T>(IEnumerable<T> itemsEnumerable, Func<T, int> weightKey) {
+            return Get(itemsEnumerable, weightKey, Random);
+        }
+        
+        public static T Get<T>(IEnumerable<T> itemsEnumerable, Func<T, int> weightKey, Random rng) {
             // save on new list allocation as, very likely, we're already passing a list
             var items = itemsEnumerable as IList<T> ?? itemsEnumerable.ToList();
             
             var totalWeight = items.Sum(weightKey);
-            var targetWeight = Random.Next(totalWeight);
+            var targetWeight = rng.Next(totalWeight);
             var accumulatedWeight = 0;
             for (var i = 0; i < items.Count; i++) {
                 var item = items[i];
@@ -23,6 +27,13 @@ namespace Broilerplate.Tools {
                 }
             }
             return default;
+        }
+
+        public static T RandomWithWeight<T>(this IEnumerable<T> itemsEnumerable, Func<T, int> weightKey, Random rng) {
+            var enumerable = itemsEnumerable as IList<T> ?? itemsEnumerable.ToList();
+            return enumerable.Count == 0 ? default :
+                // and in here we don't create a new list since this already is a list.
+                Get(enumerable, weightKey, rng);
         }
     }
 }
