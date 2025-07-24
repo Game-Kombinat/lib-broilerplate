cb35518e9942d9ab7f6a0728bd207c8ce6efb6a7
Chris <christoph.ksoll@sparkgamesstudio.com>
2025-07-24 12:56:45 +0200

    Parallel selector and sequence for BT
    Pretty neat, I feel like.

---
+++ Runtime/Broilerplate/Tools/Bt/ParallelSelector.cs
@@ -0,0 +1,38 @@
+﻿namespace Broilerplate.Tools.Bt {
+    /// <summary>
+    /// Runs all children as a selector in parallel.
+    /// Otherwise acts like a selector.
+    /// </summary>
+    public class ParallelSelector : Node {
+        public ParallelSelector(string name) : base(name) {
+        }
+        
+        protected override TaskStatus Process() {
+            int failed = 0;
+            for (int i = 0; i < Children.Count; i++) {
+                var child = Children[i];
+                var childStatus = child.Status;
+                if (childStatus is TaskStatus.Success) {
+                    return TaskStatus.Success;
+                }
+                if (childStatus is TaskStatus.Failure or TaskStatus.Terminated) {
+                    failed++;
+                }
+
+            }
+
+            if (failed == Children.Count) {
+                return TaskStatus.Failure;
+            }
+            // none was selected
+            return TaskStatus.Running;
+        }
+
+        public override void Spawn() {
+            base.Spawn();
+            for (int i = 0; i < Children.Count; i++) {
+                Children[i].Spawn();
+            }
+        }
+    }
+}
---
+++ Runtime/Broilerplate/Tools/Bt/ParallelSequence.cs
@@ -0,0 +1,38 @@
+﻿namespace Broilerplate.Tools.Bt {
+    /// <summary>
+    /// Runs all children as a sequence in parallel.
+    /// Otherwise acts like a sequence.
+    /// </summary>
+    public class ParallelSequence : Node {
+        public ParallelSequence(string name) : base(name) {
+        }
+
+        protected override TaskStatus Process() {
+            int succeeded = 0;
+            for (int i = 0; i < Children.Count; i++) {
+                var child = Children[i];
+                var childStatus = child.Status;
+                if (childStatus is TaskStatus.Failure or TaskStatus.Terminated) {
+                    return TaskStatus.Failure;
+                }
+
+                if (childStatus == TaskStatus.Success) {
+                    succeeded++;
+                }
+            }
+
+            if (succeeded == Children.Count) {
+                return TaskStatus.Success;
+            }
+
+            return TaskStatus.Running;
+        }
+
+        public override void Spawn() {
+            base.Spawn();
+            for (int i = 0; i < Children.Count; i++) {
+                Children[i].Spawn();
+            }
+        }
+    }
+}
